33fcdb532f62d04f92e728b4c3c04d520a3ba6be
Carlosguggiana <70643548+Carlosguggiana@users.noreply.github.com>
2023-04-17 00:11:26 -0300

    Update PaginaSignin.vue

--- src/pages/PaginaSignin.vue
+++ src/pages/PaginaSignin.vue
@@ -1,1 +1,54 @@
-<template>SignIn</template>
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+    title="User Registration"
+  >
+    <v-container>
+      <v-text-field
+        v-model="first"
+        color="primary"
+        label="Primeiro nome"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="last"
+        color="primary"
+        label="Sobrenome"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        color="primary"
+        label="Email"
+        variant="underlined"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="password"
+        color="primary"
+        label="Senha"
+        placeholder="Enter your password"
+        variant="underlined"
+      ></v-text-field>
+
+    
+      
+    </v-container>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+
+      <v-btn color="success">
+        Complete Registration
+
+        <v-icon icon="mdi-chevron-right" end></v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
